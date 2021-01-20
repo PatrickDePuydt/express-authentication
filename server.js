5ecb13138ba8270ecdded6ec4637ae0f6d26fac2
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-20 15:21:01 -0700

    master: init session

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+app.use(session({
+  secret: 'blahblahblahblahblah',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/profile', (req, res) => {
+  
   res.render('profile');
 });
 

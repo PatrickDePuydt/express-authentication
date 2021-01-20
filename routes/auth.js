0e9225a922041fcccca8234764e94d1bc778f977
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-20 14:40:47 -0700

    master: commit pseudocode

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 // Everything is mounted on /auth
 
@@ -13,7 +14,25 @@ router.get('/login', (req, res) => {
 
 // Sign Up POST route
 router.post('/signup', (req, res) => {
-  res.send(req.body)
+  // Find or Create a new user based on the email (or private key)
+
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password      
+    }
+  }).then(([user, created]) => {
+    //// If user was created
+    ////// Redirect to homepage or profile
+    //// else there was NOT a user created
+    ////// redirect to auth/signup
+  });
+
+
+  // res.send(req.body)
 });
 
 module.exports = router;
